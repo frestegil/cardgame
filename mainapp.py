391c578cd59a21e4105b227f030c72e7bd696232
fmounier <frederic.mounier@gmail.com>
2022-12-27 14:12:20 +0100

    update file uploader

--- mainapp.py
+++ mainapp.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 
 st.title('Card game')
-file = st.upload('Select file to upload')
+file = st.file_uploader('Select file to upload')
 df = pd.read_csv(file, sep=";")
 st.write (df.head())
