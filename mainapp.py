0dcc8ba0cff19632c0ff4bd639fe4769e07d5683
fmounier <frederic.mounier@gmail.com>
2022-12-28 18:14:28 +0100

    fdf

--- mainapp.py
+++ mainapp.py
@@ -12,11 +12,12 @@ if file != '':
 	selectedCheckboxes = [x for x, z in enumerate(checkboxes) if z == True] 
 	selectedLevels = levels.iloc[selectedCheckboxes]
 	selectedWords = df.merge(selectedLevels, on='niveau')
+	
 	# Play
-	st.button('New word',
-		on_click=selectplayer,
-		args=(selectedWords.sample(n=1)['mot'].values[0],30,)):
+	st.button('New word', on_click=new_word_click, args=(selectedWords, 30,)):
 
 
-def new_word_click(word, timer):
+def new_word_click(words, timer):
+	word = words.sample(n=1)['mot'].values[0]
 	st.write(f'# {word}')
+	st.write(f'{timer}')
