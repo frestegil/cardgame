be8c63a0b68e75d6c918255428b4d508003b70ba
fmounier <frederic.mounier@gmail.com>
2022-12-28 18:57:53 +0100

    t11

--- mainapp.py
+++ mainapp.py
@@ -31,4 +31,4 @@ if file != '':
 	for percent_complete in range(st.session_state.timer):
 		time.sleep(1)
 		rem_time = (st.session_state.timer - percent_complete)/st.session_state.timer
-		my_bar.progress(percent_complete - 1)
+		my_bar.progress(rem_time)
