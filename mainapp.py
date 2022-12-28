4e5282a77dd3fefa16b301b5b1b4ea59e8f6ad6d
fmounier <frederic.mounier@gmail.com>
2022-12-28 11:35:13 +0100

    correct bug

--- mainapp.py
+++ mainapp.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title('Card game')
 file = st.file_uploader('Select file to upload')
-df = pd.Dataframe({"caterogie": [], "niveau": [], "mot":[]})
+df = pd.DataFrame({"caterogie": [], "niveau": [], "mot":[]})
 if file != '':
 	df = pd.read_csv(file, sep=";")
 	st.write (df.head())
