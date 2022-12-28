95c4f72e13c2b1c36a5c5d33a153441e987b8398
fmounier <frederic.mounier@gmail.com>
2022-12-28 11:34:08 +0100

    Filter

--- mainapp.py
+++ mainapp.py
@@ -3,5 +3,11 @@ import pandas as pd
 
 st.title('Card game')
 file = st.file_uploader('Select file to upload')
-df = pd.read_csv(file, sep=";")
-st.write (df.head())
+df = pd.Dataframe({"caterogie": [], "niveau": [], "mot":[]})
+if file != '':
+	df = pd.read_csv(file, sep=";")
+	st.write (df.head())
+levels = df.niveau.unique()
+x = [st.checkbox(levels)]
+filtered_words = df.loc[df['niveau'].isin(x),:]
+st.write(filtered_words)
